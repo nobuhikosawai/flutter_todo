6b3382e73c8a70333725999b2f40dcb5d9874021
Nobuhiko Sawai <nobuhiko.sawai@gmail.com>
2021-03-09 03:54:58 +0900

    feat(todo): call controller from screen

--- lib/ui/screens/todo_screen/todo_screen.dart
+++ lib/ui/screens/todo_screen/todo_screen.dart
@@ -1,59 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_example/ui/common/custom_color.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../common/custom_color.dart';
+import '../../controllers/me_controller.dart';
+import '../../controllers/todo_controller.dart';
 import 'todo_input_field.dart';
 
 class TodoScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    final todos = useState([
-      {'key': '1', 'title': 'TODO1', 'completed': false},
-      {'key': '2', 'title': 'TODO2', 'completed': true},
-    ]);
+    final me = useProvider(meProvider.state);
+    final meId = me.data?.value?.id;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Your Awesome Todos!'),
-        backgroundColor: CustomColor.primary,
-      ),
-      body: ReorderableListView(
-        onReorder: (oldIndex, newIndex) {
-          if (oldIndex < newIndex) {
-            newIndex -= 1;
-          }
-          final todosDup = [...todos.value];
-          final item = todosDup.removeAt(oldIndex);
-          todosDup.insert(newIndex, item);
-          todos.value = todosDup;
+    if (meId == null) Container();
+
+    final todos = useProvider(todoProvider(meId).state);
+
+    return todos.when(
+        data: (todos) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Your Awesome Todos!'),
+              backgroundColor: CustomColor.primary,
+            ),
+            body: ReorderableListView(
+              onReorder: (oldIndex, newIndex) {
+                // FIXME
+                // if (oldIndex < newIndex) {
+                //   newIndex -= 1;
+                // }
+                // final todosDup = [...todos.data.value];
+                // final item = todosDup.removeAt(oldIndex);
+                // todosDup.insert(newIndex, item);
+                // todos.data.value = todosDup;
+              },
+              children: [
+                for (final todo in todos)
+                  CheckboxListTile(
+                      key: Key(todo.id),
+                      controlAffinity: ListTileControlAffinity.leading,
+                      activeColor: CustomColor.primary,
+                      title: Text(todo.title),
+                      value: todo.completed,
+                      onChanged: (value) {})
+              ],
+            ),
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: CustomColor.primary,
+              onPressed: () {
+                showModalBottomSheet<void>(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(16.0),
+                          topRight: Radius.circular(16.0)),
+                    ),
+                    context: context,
+                    builder: (buildContext) {
+                      return TodoInputField();
+                    });
+              },
+              child: Icon(Icons.add),
+            ),
+          );
         },
-        children: [
-          for (final todo in todos.value)
-            CheckboxListTile(
-                key: Key(todo['key'] as String),
-                controlAffinity: ListTileControlAffinity.leading,
-                activeColor: CustomColor.primary,
-                title: Text(todo['title'] as String),
-                value: todo['completed'] as bool,
-                onChanged: (value) {})
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: CustomColor.primary,
-        onPressed: () {
-          showModalBottomSheet<void>(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(16.0),
-                    topRight: Radius.circular(16.0)),
-              ),
-              context: context,
-              builder: (buildContext) {
-                return TodoInputField();
-              });
-        },
-        child: Icon(Icons.add),
-      ),
-    );
+        loading: () => Center(child: CircularProgressIndicator()),
+        error: (error, stackTrace) {
+          return Container();
+        });
   }
 }
